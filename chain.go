770877d5ee19cb9fd524eb5652bcf0202871648f
Tyler Boright <ru.lai.development@gmail.com>
2020-01-12 18:33:31 -0800

    changes the api to have better naming conventions; adds the method to Predict the next state using the randomutil package; adds unit tests to cover broader user cases

--- chain.go
+++ chain.go
@@ -1,6 +1,7 @@
 package chain
 
 import (
+	"fmt"
 	ru "github.com/jmcvetta/randutil"
 )
 
@@ -8,23 +9,35 @@ type MarkovChain struct {
 	States map[string][]ru.Choice
 }
 
-func (mc *MarkovChain) AddStateChoice(key, state string, prob int) {
+func (mc *MarkovChain) SetState(key, state string, prob int) {
+	// If the key doesn't exist, append the key to the state transition graph
 	if _, ok := mc.States[key]; ok == false {
 		mc.States[key] = append(mc.States[key], ru.Choice{prob, state})
 	} else {
-		var desiredIndex int
+		// Use struct to store if the int has been set or not
+		// and the index where the value was found
+		var sStatus struct {
+			HasBeenSet bool
+			Idx        int
+		}
+
 		ss := mc.States[key]
 		for i := 0; i < len(ss); i++ {
 			if ss[i].Item == state {
-				desiredIndex = i
+				sStatus.HasBeenSet = true
+				sStatus.Idx = i
 			}
 		}
 
-		mc.States[key][desiredIndex] = ru.Choice{prob, state}
+		if sStatus.HasBeenSet {
+			mc.States[key][sStatus.Idx] = ru.Choice{prob, state}
+		} else {
+			mc.States[key] = append(mc.States[key], ru.Choice{prob, state})
+		}
 	}
 }
 
-func (mc *MarkovChain) Insert(key, state string) {
+func (mc *MarkovChain) IncrementState(key, state string) {
 	// check for key and add key and key and state if they both don't exist
 	if _, ok := mc.States[key]; ok == false {
 		mc.States[key] = append(mc.States[key], ru.Choice{1, state})
@@ -38,12 +51,25 @@ func (mc *MarkovChain) Insert(key, state string) {
 		}
 
 		if desiredState == nil {
-			mc.States[key] = append(ss, ru.Choice{1, state})
+			mc.States[key] = append(mc.States[key], ru.Choice{1, state})
 		} else {
 			desiredState.Weight += 1
 		}
 
 	}
+}
+
+func (mc *MarkovChain) PredictState(key string) (string, error) {
+	if _, ok := mc.States[key]; ok == false {
+		return "", fmt.Errorf("You have entered a key that does not exist. Add that key to the chain and run again.")
+	}
+
+	c, err := ru.WeightedChoice(mc.States[key])
+	if err != nil {
+		return "", fmt.Errorf("The weighted random selection failed.  Check and ensure your MarkovChain has no negative values and at least one non-zero value.")
+	}
+
+	return c.Item.(string), nil
 }
 
 func NewChain() MarkovChain {
--- chain_test.go
+++ chain_test.go
@@ -21,7 +21,57 @@ type Args struct {
 	Probability int
 }
 
-func TestInsert(t *testing.T) {
+func TestSetState(t *testing.T) {
+	mc := NewChain()
+	tests := []struct {
+		Args                 Args
+		ExpectedChoiceLength int
+		Expected             ru.Choice
+		Name                 string
+	}{
+		{
+			Args:                 Args{"a", "b", 1},
+			Expected:             ru.Choice{Weight: 1, Item: "b"},
+			ExpectedChoiceLength: 1,
+			Name:                 "SetState adds a state when the chain doesn't contain the key or value",
+		},
+		{
+			Args:                 Args{"a", "c", 4},
+			ExpectedChoiceLength: 2,
+			Expected:             ru.Choice{Weight: 4, Item: "c"},
+			Name:                 "SetState adds a state when the chain has the key and not the value",
+		},
+		{
+			Args:                 Args{"a", "b", 12},
+			ExpectedChoiceLength: 2,
+			Expected:             ru.Choice{Weight: 12, Item: "b"},
+			Name:                 "SetState sets the Weight to a specific one even when the node was previously set.",
+		},
+	}
+	for _, test := range tests {
+		testArgs := test.Args
+		mc.SetState(testArgs.Key, testArgs.State, testArgs.Probability)
+
+		var actual ru.Choice
+		for i := 0; i < len(mc.States[testArgs.Key]); i++ {
+			if mc.States[testArgs.Key][i].Item == test.Expected.Item {
+				actual = mc.States[testArgs.Key][i]
+			}
+		}
+
+		if actual.Weight != test.Expected.Weight {
+			t.Errorf("%s failed SetState with the following results:\nactual: %v, expected: %v\n", test.Name, actual.Weight, test.Expected.Weight)
+		}
+
+		actualChoiceLength := len(mc.States[testArgs.Key])
+		if actualChoiceLength != test.ExpectedChoiceLength {
+			t.Errorf("The length of Choices is incorrect for the following test: %s", test.Name)
+		}
+
+	}
+}
+
+func TestIncrementState(t *testing.T) {
 	mc := NewChain()
 	tests := []struct {
 		Args     Args
@@ -48,7 +98,7 @@ func TestInsert(t *testing.T) {
 	for _, test := range tests {
 		testArgs := test.Args
 
-		mc.Insert(testArgs.Key, testArgs.State)
+		mc.IncrementState(testArgs.Key, testArgs.State)
 
 		var actual ru.Choice
 		for i := 0; i < len(mc.States[testArgs.Key]); i++ {
@@ -58,47 +108,48 @@ func TestInsert(t *testing.T) {
 		}
 
 		if actual.Weight != test.Expected.Weight {
-			t.Errorf("%s failed with the following results:\nactual: %v, expected: %v", test.Name, actual, test.Expected)
+			t.Errorf("%s failed with the following results:\nactual: %v, expected: %v\n", test.Name, actual, test.Expected)
 		}
 	}
 }
 
-func TestAddStateChoice(t *testing.T) {
+func TestPredictState(t *testing.T) {
 	mc := NewChain()
-	tests := []struct {
-		Args     Args
-		Expected ru.Choice
-		Name     string
-	}{
-		{
-			Args:     Args{"a", "b", 1},
-			Expected: ru.Choice{Weight: 1, Item: "b"},
-			Name:     "AddStateChoice adds a state when the chain doesn't contain the key or value",
-		},
-		{
-			Args:     Args{"a", "c", 4},
-			Expected: ru.Choice{Weight: 4, Item: "c"},
-			Name:     "AddStateChoice adds a state when the chain has the key and not the value",
-		},
-		{
-			Args:     Args{"a", "b", 12},
-			Expected: ru.Choice{Weight: 12, Item: "b"},
-			Name:     "AddStateChoice sets the Weight to a specific one even when the node was previously set.",
-		},
+	mc.IncrementState("a", "b")
+	mc.IncrementState("a", "b")
+	mc.SetState("a", "c", 0)
+	mc.IncrementState("b", "c")
+
+	expected := "b"
+	actual, err := mc.PredictState("a")
+	if err != nil {
+		t.Error(err.Error())
 	}
-	for _, test := range tests {
-		testArgs := test.Args
-		mc.AddStateChoice(testArgs.Key, testArgs.State, testArgs.Probability)
 
-		var actual ru.Choice
-		for i := 0; i < len(mc.States[testArgs.Key]); i++ {
-			if mc.States[testArgs.Key][i].Item == test.Expected.Item {
-				actual = mc.States[testArgs.Key][i]
-			}
-		}
+	if actual != expected {
+		t.Errorf("TestPredictState failed with the following results:\nactual: %s, expected: %s\n", actual, expected)
+	}
+}
 
-		if actual.Weight != test.Expected.Weight {
-			t.Errorf("%s failed AddStateChoice with the following results:\nactual: %v, expected: %v", test.Name, actual.Weight, test.Expected.Weight)
-		}
+func TestPredictStateFailures(t *testing.T) {
+	mc := NewChain()
+	actual, err := mc.PredictState("a")
+	if err == nil {
+		t.Errorf("TestPredictState should fail when trying to predict an empty key.  Returned the following instead:\nActual: %s, Error: %s", actual, err)
+	}
+
+	mc.SetState("a", "b", 0)
+	mc.SetState("a", "c", 0)
+	actual, err = mc.PredictState("a")
+	if err == nil {
+		t.Errorf("TestPredictState should fail when trying to predict a key with no non-zero states.  Returned the following instead:\nActual: %s, Error: %s", actual, err)
+	}
+
+	mc.SetState("a", "d", -1)
+	mc.SetState("a", "e", 1)
+
+	actual, err = mc.PredictState("a")
+	if err == nil {
+		t.Errorf("TestPredictState should fail when trying to predict a key that has a negative integer as a state.  Returned the following instead:\nActual: %s, Error: %s", actual, err)
 	}
 }
